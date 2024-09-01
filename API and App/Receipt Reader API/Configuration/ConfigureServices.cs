9555efec46c82bfa0f56d7b539d0490af6dac365
Boldizsár Németh <boldizsar.nemeth@p92.hu>
2024-09-01 19:56:37 +0200

    renamed DB

--- API and App/Receipt Reader API/Configuration/ConfigureServices.cs
+++ API and App/Receipt Reader API/Configuration/ConfigureServices.cs
@@ -5,6 +5,6 @@ public static class ConfigureServices
     public static void ConfigureDependencyInjection(this IServiceCollection services)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=PaaDB;Trusted_Connection=True;MultipleActiveResultSets=true;"));
+            options.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=RRDB;Trusted_Connection=True;MultipleActiveResultSets=true;"));
     }
 }
