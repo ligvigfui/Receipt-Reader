b53271936f9869d9fe08a2483508752af54a0464
Leves0003 <fodorlevente008@gmail.com>
2024-10-05 12:46:06 +0200

    +1 login page

---
+++ RR.Frontend/src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <input type="email" v-model="state.email">
+    <input type="password">
+    
+  </div>
+</template>
+<script setup lang="ts">
+const state = reactive({
+  email: '',
+});
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
