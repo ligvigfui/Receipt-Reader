87e463d5c058ed92bbd6cb26b35154ec8009e542
Boldizsár Németh <boldizsar.nemeth@p92.hu>
2024-10-05 13:46:09 +0200

    login works

--- RR.Frontend/src/views/LoginView.vue
+++ RR.Frontend/src/views/LoginView.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="about">
-    <input type="email" v-model="state.email">
-    <input type="password">
-    
+    <input type="email" v-model="state.email" />
+    <input type="password" v-model="state.password" />
+    <button @click="OnSubmit">Submit</button>
   </div>
 </template>
 <script setup lang="ts">
+import { reactive } from 'vue'
+
 const state = reactive({
   email: '',
-});
+  password: ''
+})
+
+async function OnSubmit() {
+  const response = await fetch('https://localhost:5001/account/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: state.email,
+      password: state.password
+    })
+  })
+  const result = await response.json()
+  if (response.ok) {
+    alert(result.token)
+  } else {
+    alert(result.message)
+  }
+}
 </script>
 <style>
 @media (min-width: 1024px) {
